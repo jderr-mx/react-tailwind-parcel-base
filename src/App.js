ad8af6944cb7e23315706212f72ad3b0c08983f4
John Derr <john.derr@maximos.io>
2020-06-11 09:59:31 -0400

    initial setup of parcel/react/tailwind base project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './styles.css';
+
+
+const App = () => {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+render(<App />, document.getElementById('root'));
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {
+      height: {
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
